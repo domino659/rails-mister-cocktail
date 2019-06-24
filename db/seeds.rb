202b27aaba8a29fa28eae34e8eee573a0e68e4fd
Martin SION <martin@sionfamily.com>
2019-06-24 18:56:40 +0200

    hei

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.create(name: "Mojito", remote_photo_url: "https://res.cloudinary.com/dxgynqjm3/image/upload/v1561387380/iy4vzndooez4xezxtecr.jpg")
+Cocktail.create(name: "Pina Colada", remote_photo_url: "https://res.cloudinary.com/dxgynqjm3/image/upload/v1561389304/Pina_Colada_ijotlu.jpg" )
+Cocktail.create(name: "Cosmopolitan", remote_photo_url: "https://res.cloudinary.com/dxgynqjm3/image/upload/v1561389304/Cosmopolitain_qhivs3.jpg" )
+Cocktail.create(name: "Gin Fizz", remote_photo_url: "https://res.cloudinary.com/dxgynqjm3/image/upload/v1561389303/GIN_Fizz_hoq0qn.jpg" )
 
 Ingredient.create(name: "Lemon")
 Ingredient.create(name: "Ice")
